f7a30387fac71cc94c70b04069b2d0fbfe433f70
Javier Sanesteban <javiersanesteban@MacBook-Air-de-Javier.local>
2019-01-07 16:37:32 +0100

    Primera versión calculadora

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// ---- Accede a la calculadora --------------------
+
+function calculadora() {
+    var num1 = parseInt(prompt("Introduce el primer operando"));
+    var num2 = parseInt(prompt("Introduce el segundo operando"))
+    var suma = (num1) + (num2);
+    var resta = num1 - num2;
+    var mult = num1 * num2;
+    var divide = num1 / num2;
+alert("La suma de los operandos es " + suma);
+alert("La resta de los operandos es " + resta)
+alert("La división de los operandos es " + divide)
+}
